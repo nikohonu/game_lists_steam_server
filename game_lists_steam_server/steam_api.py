c863b57afd89ee3cb5db7da92e3e69be25088739
nikohonu <nikohonu@gmail.com>
2022-02-18 16:56:01 +0200

    add getting user tags for game

--- game_lists_steam_server/steam_api.py
+++ game_lists_steam_server/steam_api.py
@@ -2,6 +2,7 @@
 from steam.steamid import SteamID
 from steam.webapi import WebAPI
 from requests import get
+from bs4 import BeautifulSoup
 
 
 class SteamAPI:
@@ -13,7 +14,8 @@ class SteamAPI:
         print('call get_owned_games')
         return self.web_api.IPlayerService.GetOwnedGames(steamid=steam_id, appids_filter=[], include_appinfo=True, include_free_sub=True, include_played_free_games=True)
 
-    def get_steam_id_from_url(self, profile_url: str):
+    @staticmethod
+    def get_steam_id_from_url(profile_url: str):
         print('call get_steam_id_from_url')
         return SteamID.from_url(profile_url)
 
@@ -26,5 +28,15 @@ class SteamAPI:
         print('call get_appdetails')
         response = get(
             "https://store.steampowered.com/api/appdetails", params={'appids': app_id})
-        print(response.url)
         return response.json()
+
+    @staticmethod
+    def get_app_tags(app_id):
+        print('call get_app_tags')
+        response = get(f'https://store.steampowered.com/app/{app_id}')
+        bs = BeautifulSoup(response.text)
+        app_tags = bs.find_all("a", {"class": "app_tag"})
+        tags = []
+        for tag in app_tags:
+            tags.append(tag.text.strip())
+        return tags
